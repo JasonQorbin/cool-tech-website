c6c7dc9ea82df53b04c3ca9f0571c7a6eba93240
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-03-13 00:22:49 +0200

    Terms blade

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Cool Tech | Terms of Use</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+</head>
+<body>
+<div class="content-wrap">
+    <header>
+        <x-home-button/>
+        <h1>Terms of Use</h1>
+        <span class="divider"></span>
+    </header>
+    <p>
+        These Terms of Use (“Terms”),govern your use of our website and any other service we may offer (collectively
+        "Services"). Please review these Terms carefully before using the website because they affect your rights.
+        By using this website, you accept these Terms and agree to be legally bound by them.
+    </p>
+
+    <p>Please consult our <a href="/privacy">Privacy Policy</a> for information regarding our privacy practices.</p>
+
+    <p>
+        The audio and video materials, photographs, text, graphics, logos, layouts, designs, interfaces, software, data
+        and other content associated with the Services (“Content”) are protected by intellectual property and other laws
+        in your country. You must comply with all such laws and applicable copyright, trademark or
+        other legal notices or restrictions. You shall not remove or alter any copyright, trademark, or other legal
+        notices marked on the Content.  No transfer of ownership to any portion of the Content
+        shall be made as a result of any access you are granted. Except as specifically provided below, we reserve all
+        rights in and to the Services and Content.
+    </p>
+
+    <p>
+        You are only permitted to access and view the Content for personal, non-commercial purposes in accordance with
+        these Terms, and may not build a business or other enterprise utilizing any of the Content, whether for profit
+        or not. Except where expressly authorized by us in writing, you may not either
+        directly or through the use of any software, device, internet site, web-based service or other means download,
+        stream capture, store in a database, archive or otherwise copy any part of the Services or Content; upload,
+        sell, rent, lease, lend, broadcast, transmit or otherwise disseminate, distribute, display or perform any part
+        of the Services or Content; license or sublicense any part of the Services or Content; or in any way exploit any
+        part of the Services or Content. In addition, except where expressly
+        authorized by us in writing, you are strictly prohibited from modifying Content; creating, distributing or
+        advertising an index of any significant portion of the Content; or otherwise creating derivative works or
+        materials that otherwise are derived from or based in any way on the Content, including, without limitation,
+        mash-ups and similar videos, montages, translations, desktop themes, fonts, icons, wallpaper, greeting cards,
+        and merchandise.
+    </p>
+    <x-cookie-notice/>
+    <x-page-footer/>
+</div>
+</body>
+</html>
