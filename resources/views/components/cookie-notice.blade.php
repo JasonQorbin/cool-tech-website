5686abc74d6386393ecfdd9d528c323f2ba07083
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-03-13 00:31:52 +0200

    Cookie notice component

---
+++ resources/views/components/cookie-notice.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <hr>
+    <p class="feint-text">Cookie notice: Note that this site uses minimal cookies to ensure proper functioning of the site. There are no
+        tracking or advertising cookies used. By continuing to use the site you are consenting to the use of these cookies.</p>
+</div>
