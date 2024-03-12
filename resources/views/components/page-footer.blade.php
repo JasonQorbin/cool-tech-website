3c4f7a5ca7a568e3f6bfdb921f774425b15fa502
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-03-13 00:29:34 +0200

    Page footer component

---
+++ resources/views/components/page-footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <span class="divider"></span>
+    <div class="side-by-side">
+        <span class="left">Legal: <a href="/terms">Terms of service</a> | <a href="/privacy">Privacy policy</a></span>
+        <span class="right">Copyright © {{Date('Y')}} CoolTech</span>
+    </div>
+</footer>
