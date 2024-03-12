ff092eb995c2bfe700706f51bc9da387b620827b
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-03-13 00:19:38 +0200

    Privacy blade

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Cool Tech | Privacy Policy</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+</head>
+<body>
+<div class="content-wrap">
+    <header>
+        <x-home-button/>
+        <h1>Privacy Policy</h1>
+        <span class="divider"></span>
+    </header>
+    <p>
+        At Cool Tech we pride ourselves on being your trusted source of technology related media.
+    <p>
+        We know how important it is to protect your personal information. This privacy policy explains our practices
+        (and your choices) on how we collect, use, and share your information when you interact with any of our
+        website and media.
+    </p>
+    <h2>What information we collect</h2>
+    <p>
+        We collect your information when you sign up for our services or create and account on our website. We also
+        collect information about how you use our services. We may also receive information about you from third
+        parties. This information can identify you or your device, or can be linked to you.
+    </p>
+
+    <h3>Information provided by you</h3>
+    <h4>Registration information</h4>
+    <p>
+        When you sign up for our services, we collect the information you provide, such as your name
+        and email address.
+    </p>
+
+    <h4>Information and content you post</h4>
+    <p>
+        When you post or share comments or other content when using our services, we may collect information such as your
+        username, screen name, comments, likes, interests, status, and pictures. We may also collect audio or visual
+        content, such as photos, videos, or recordings you choose to post when using our services, or when granting us
+        access to your camera or microphone while using interactive features of our services.
+    </p>
+
+    <h4>Your messages and feedback</h4>
+
+    <p>
+        We may collect the information you provide when you contact us directly or provide feedback, comments, or
+        suggestions on our services.
+    </p>
+    <h3>Information collected automatically</h3>
+    <p>
+        When using our services, we may automatically collect or receive certain information associated with you or
+        your devices. The types of information we may collect automatically include your use of our services, your
+        preferences, and how you interact with our content. It may also include device identifiers such as IP address,
+        cookie identifiers, media access control (MAC) address, and other unique identifiers.
+    <h2>How we use your information</h2>
+    <p>
+        We may process your personal information to provide you with the services you request or to maintain our
+        relationship with you. We also may process your personal information for legitimate business purposes, or where
+        the law requires or permits us to do so. We will ask for your consent beforehand if we need it to process
+        your personal information.
+    </p>
+    <h3>Providing you access to Cool Tech services</h3>
+    <p>We may use your personal information to give you access to our services. We may also process your personal
+        information to manage our relationship with you. These activities may include the following:
+    </p>
+    <ul>
+        <li>Creating and managing your account, subscriptions, and registrations.</li>
+        <li>Authenticating your account or verifying your identity.</li>
+        <li>Providing you with the services that you request, customer support, and account updates.</li>
+        <li>Completing any transactions you request.</li>
+    </ul>
+    <h3>Legitimate business purposes</h3>
+    <p>We may use your personal information for the following legitimate business purposes:</p>
+    <ul>
+        <li>Providing and managing our services, and ensuring that our services function correctly.</li>
+        <li>Maintaining and enhancing the safety and security of our services and preventing misuse.</li>
+        <li>Communicating with you and responding to your questions.</li>
+        <li>Personalizing your experience with our services.</li>
+        <li>Managing contests, giveaways, competitions, sweepstakes, offers, or promotions, including verifying your
+            eligibility and selecting winners.</li>
+        <li>Performing social media activities, market research, surveys, and product testing.</li>
+        <li>Running advertising and marketing campaigns for our own or third-party products and services, including by
+            sending promotional messages and newsletters, and creating user segments to target more relevant audiences
+            (which may involve converting your information to hashed identifiers).</li>
+        <li>Managing social media interactions and user-generated content (such as posts or comments you submit on public
+            platforms), enabling interactive features, and personalizing our services.</li>
+        <li>Managing our audience relationships, shows, and events.</li>
+        <li>Operating, analyzing, and improving our services (including determining and managing the effectiveness of our
+            advertising and marketing; performing data analytics, research and optimization; learning more about our
+            audiences; and performing accounting, auditing, and other internal functions).</li>
+        <li>Exercising our rights and remedies and defending ourselves against legal claims.</li>
+        <li>Preventing fraud and other illegal activity, claims, and other liabilities.</li>
+        <li>Complying with and enforcing relevant industry standards, our terms, policies, and other rights.</li>
+    </ul>
+
+    <h3>Legal obligations</h3>
+    <p>We may use your personal information to comply with our legal obligations, including performing accounting and
+        tax-related obligations, and responding to government and judicial requests.</p>
+    <p>We also may process your personal information when the law otherwise permits.</p>
+
+    <h3>With your consent</h3>
+    <p>In all other cases, we will ask for your consent beforehand if we need it to process your personal
+        information.</p>
+
+    <h2>Who we share your information with</h2>
+    <p>
+        We may share the categories of personal information we obtain about you (as described above) with the categories
+        of third parties listed in this section. We may share your personal information  with our service providers,
+        and with our business partners (including providing advertising based on your interests).
+    </p>
+    <h2>Your rights and choices</h2>
+    <p>
+        You may have certain rights and choices regarding your personal information, such as the right to ask us for a
+        copy of the information we hold about you. These rights are not available everywhere and may be limited by local
+        laws. For example, these rights may not be available to you if fulfilling your request would reveal personal
+        information about another person, or if you ask us to delete information which we are required by law to
+        keep.
+    </p>
+
+    <h2>Data retention</h2>
+    <p>
+        We will retain your personal information for the time period reasonably necessary to achieve the purposes
+        described in this privacy policy, or any other notice provided at the time of collection unless a longer
+        retention period is required or permitted by applicable law, taking into account applicable statutes of
+        limitation and our records retention requirements and policies.
+    </p>
+
+    <h2>Cookies and tracking technologies</h2>
+    <p>
+        We may automatically receive certain information about you and your device using browser- or device-based
+        tracking technologies described below. These technologies allow us to ensure our services are functioning
+        correctly, improve our services, provide and measure advertisements, and provide personalized content to
+        you.
+    </p>
+
+    <p>
+        You can manage how these tracking technologies are used by adjusting your device or browser settings and, in some
+        countries or by selecting your preferences in the cookie management pop-up.
+    </p>
+
+    <h2>Changes to this policy</h2>
+    <p>
+        We may update this privacy policy based on new technologies, industry practices, regulatory requirements,
+        or for other purposes. We will let you know if these changes are material and, where required by applicable law,
+        we will obtain your consent. When updating you about any material changes, we may email you at the last email
+        address you provided, post a notice on our websites or applications, or by any other means permitted by
+        applicable law.
+    </p>
+    <x-cookie-notice/>
+    <x-page-footer/>
+</div>
+</body>
+</html>
