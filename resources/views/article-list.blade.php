62c510f692b03eda2c24cb82d9df85df699d73b7
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-03-13 00:17:25 +0200

    Article list blade

---
+++ resources/views/article-list.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Cool Tech | Results</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+</head>
+<body>
+<div class="content-wrap">
+    <header>
+        <x-home-button/>
+        <h1>Search results: {{$searchTerm}}</h1>
+        <span class="divider"></span>
+    </header>
+    <div id="tile-list">
+        @foreach($articles as $article)
+            <a href="/article/{{$article->id}}"><div class="article-tile"><h2>{{$article->title}}</h2></div></a>
+        @endforeach
+
+    </div>
+    <x-cookie-notice/>
+    <x-page-footer/>
+</div>
+</body>
+</html>
